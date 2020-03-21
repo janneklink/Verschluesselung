44c7c93efb285c78d76373b3d1280ebb063b4f04
janneklink <linkjannek@gmail.com>
2020-03-21 12:37:06 +0100

    CaesarCipher finished

--- src/main/java/caesar/CaesarCipher.java
+++ src/main/java/caesar/CaesarCipher.java
@@ -2,7 +2,42 @@ package caesar;
 
 public class CaesarCipher {
 
-    public static String standardCaesarCipher(String cleartext, int caesarShift){
-        return cleartext;
+    public static String caesarCipher(String cleartext, int caesarShift) {
+        if (caesarShift == 0) {
+            return cleartext;
+        } else {
+            String encryptedtext = "";
+            for (char c : cleartext.toCharArray()) {
+                encryptedtext += shiftChar(c, caesarShift);
+            }
+            return encryptedtext;
+        }
+    }
+
+    private static char shiftChar(char c, int shift) {
+        return keepCharachterInLetterRange(c + shift);
+    }
+
+    //Because we only want normal letters, it has to be checked if value is actually a letter as a char
+    private static char keepCharachterInLetterRange(int letter) {
+        if (letter > charValues.Z.value) {
+            letter = (char) (letter - (charValues.Z.value + 1) + charValues.A.value);
+            if (letter >= charValues.Z.value) {
+                letter = keepCharachterInLetterRange(letter);
+            }
+        }
+        return (char) letter;
+    }
+
+
+}
+
+enum charValues {
+    A(97), Z(122);
+
+    int value;
+
+    charValues(int value) {
+        this.value = value;
     }
 }
--- src/test/java/caesar/CaesarCipherTest.java
+++ src/test/java/caesar/CaesarCipherTest.java
@@ -5,16 +5,54 @@ import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
 import net.jqwik.api.constraints.AlphaChars;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 public class CaesarCipherTest {
     //CaesarCipher(CC)(String,0) : String
     @Property
-    void testNullShift(@ForAll @AlphaChars String string ){
-        boolean inputEqualsOutput = CaesarCipher.standardCaesarCipher(string,0)==string;
+    void nullShiftProperty(@ForAll @AlphaChars String string ){
+        boolean inputEqualsOutput = CaesarCipher.caesarCipher(string,0)==string;
         Assertions.assertTrue(inputEqualsOutput);
     }
     //CC(a,1):b
+    @Test
+    void testOneShift(){
+        Assertions.assertEquals(CaesarCipher.caesarCipher("a",1),"b");
+    }
     //CC(a,9):j
+    @Test
+    void testNineShift() {
+        Assertions.assertEquals(CaesarCipher.caesarCipher("a",9),"j");
+    }
     //CC(a,26):a
+    @Test
+    void testFullShift() {
+        Assertions.assertEquals(CaesarCipher.caesarCipher("a",26),"a");
+    }
+
     //CC(a,27):b
+    @Test
+    void testMultipleShift() {
+        Assertions.assertEquals(CaesarCipher.caesarCipher("a",27),"b");
+    }
+
+    //CC(abc,1):bcd
+    @Test
+    void testMultipleCharachterShift() {
+        Assertions.assertEquals(CaesarCipher.caesarCipher("abc",1),"bcd");
+    }
+
+    //CC(faab,3):idde
+    @Test
+    void testMultipleNonSequentialCharactersShift() {
+        Assertions.assertEquals(CaesarCipher.caesarCipher("faab",3),"idde");
+    }
+
+    //CC(z,27):a
+    @Test
+    void testEdge() {
+        Assertions.assertEquals(CaesarCipher.caesarCipher("z",27),"a");
+    }
+
 }
